9c5bb58883246a689f03c64e0a07d2e189210658
yousefahmed <yousefa.mohamed12@gmail.com>
2021-01-03 05:24:40 +0200

    add validate page

---
+++ project/src/java/validate.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author yousefahmed
+ */
+@WebServlet(urlPatterns = {"/validate"})
+public class validate extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3305/hotel_reservation_database";
+            String user = "root";
+            String databasePassword = "12345678";
+            Connection Con = null;
+            PreparedStatement Stmt = null;
+            Con = DriverManager.getConnection(url, user, databasePassword);
+            HttpSession session = request.getSession(true);
+
+            Stmt = Con.prepareStatement("SELECT * FROM hotel_reservation_database.users WHERE username = ? AND password = ? ;");
+
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            Stmt.setString(1,username);
+            Stmt.setString(2,password);
+            ResultSet RS = Stmt.executeQuery();
+
+            if(!RS.next() ){
+
+                response.sendRedirect("login.html");
+                
+            }
+            else{
+                session.setAttribute("session_username", username);
+                response.sendRedirect("customerhome.jsp”");
+            }
+            RS.close();
+            Stmt.close();
+            Con.close();
+
+        }
+        catch (Exception ex) {
+                ex.printStackTrace();
+                out.print(ex);
+        }
+       
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         try {
+             processRequest(request, response);
+         } catch (ClassNotFoundException ex) {
+             Logger.getLogger(validate.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SQLException ex) {
+             Logger.getLogger(validate.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         try {
+             processRequest(request, response);
+         } catch (ClassNotFoundException ex) {
+             Logger.getLogger(validate.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SQLException ex) {
+             Logger.getLogger(validate.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
